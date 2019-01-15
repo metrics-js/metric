c7da91863a2ddec859db3b0ac8f0b23cb99bb635
Trygve Lie <post@trygve-lie.com>
2019-01-16 00:29:09 +0100

    Added validation

--- test/is.js
+++ test/is.js
@@ -3,45 +3,220 @@
 const tap = require('tap');
 const is = require('../lib/is');
 
+tap.test('is.isString() - is a String - should return true', (t) => {
+    t.true(is.isString('aa'));
+    t.true(is.isString(''));
+    t.true(is.isString('10'));
+    t.true(is.isString('true'));
+    t.true(is.isString('[1]'));
+    t.end();
+});
+
+tap.test('is.isString() - not a String - should return false', (t) => {
+    t.false(is.isString(NaN));
+    t.false(is.isString(10));
+    t.false(is.isString({ foo: 1 }));
+    t.false(is.isString(true));
+    t.false(is.isString([1]));
+    t.end();
+});
+
+tap.test('is.isEmptyString() - is a empty String - should return true', (t) => {
+    t.true(is.isEmptyString(''));
+    t.true(is.isEmptyString('  ')); // spaces
+    t.true(is.isEmptyString('   ')); // tab
+    t.true(is.isEmptyString('\n')); // newline
+    t.end();
+});
+
+tap.test('is.isEmptyString() - not an empty String - should return false', (t) => {
+    t.false(is.isEmptyString('a'));
+    t.false(is.isEmptyString('  a    '));
+    t.end();
+});
+
+tap.test('is.notEmpty() - is not empty - should return true', (t) => {
+    t.true(is.notEmpty('a'));
+    t.true(is.notEmpty('undefined'));
+    t.true(is.notEmpty('  a    '));
+    t.true(is.notEmpty('null'));
+    t.end();
+});
+
+tap.test('is.notEmpty() - is empty - should return false', (t) => {
+    t.false(is.notEmpty());
+    t.false(is.notEmpty(undefined));
+    t.false(is.notEmpty(null));
+    t.false(is.notEmpty(''));
+    t.false(is.notEmpty('  ')); // spaces
+    t.false(is.notEmpty('   ')); // tab
+    t.false(is.notEmpty('\n')); // newline
+    t.end();
+});
+
 tap.test('is.validName() - has chars in the specter of [a-zA-Z_][a-zA-Z0-9_]* - should return true', (t) => {
-    const result = is.validName('abc_def_G_01');
-    t.true(result);
+    t.true(is.validName('abc_def_G_01'));
     t.end();
 });
 
 tap.test('is.validName() - value is just a number - should return false', (t) => {
-    const result = is.validName('10');
-    t.false(result);
+    t.false(is.validName('10'));
     t.end();
 });
 
 tap.test('is.validType() - value is 0 - should return true', (t) => {
-    const result = is.validType(0);
-    t.true(result);
+    t.true(is.validType(0));
     t.end();
 });
 
 tap.test('is.validType() - value is within the range of 0 and 7 - should return true', (t) => {
-    const result = is.validType(5);
-    t.true(result);
+    t.true(is.validType(5));
     t.end();
 });
 
 tap.test('is.validType() - value is 7 - should return true', (t) => {
-    const result = is.validType(7);
-    t.true(result);
+    t.true(is.validType(7));
     t.end();
 });
 
 tap.test('is.validType() - value is lower than 0 - should return false', (t) => {
-    const result = is.validType(-1);
-    t.false(result);
+    t.false(is.validType(-1));
     t.end();
 });
 
 tap.test('is.validType() - value is higher than 7 - should return false', (t) => {
-    const result = is.validType(8);
-    t.false(result);
+    t.false(is.validType(8));
     t.end();
 });
 
+tap.test('is.validDescription() - value is a String - should return true', (t) => {
+    t.true(is.validDescription('foo'));
+    t.end();
+});
+
+tap.test('is.validDescription() - value is "null" - should return true', (t) => {
+    t.true(is.validDescription(null));
+    t.end();
+});
+
+tap.test('is.validDescription() - value is "undefined" - should return true', (t) => {
+    t.true(is.validDescription(undefined));
+    t.end();
+});
+
+tap.test('is.validDescription() - value is a Number - should return false', (t) => {
+    t.false(is.validDescription(10));
+    t.end();
+});
+
+tap.test('is.validSource() - value is a String - should return true', (t) => {
+    t.true(is.validSource('foo'));
+    t.end();
+});
+
+tap.test('is.validSource() - value is "null" - should return true', (t) => {
+    t.true(is.validSource(null));
+    t.end();
+});
+
+tap.test('is.validSource() - value is "undefined" - should return true', (t) => {
+    t.true(is.validSource(undefined));
+    t.end();
+});
+
+tap.test('is.validSource() - value is a Number - should return false', (t) => {
+    t.false(is.validSource(10));
+    t.end();
+});
+
+tap.test('is.validTimestamp() - value is a Integer - should return true', (t) => {
+    t.true(is.validTimestamp(10));
+    t.end();
+});
+
+tap.test('is.validTimestamp() - value is a Double - should return true', (t) => {
+    t.true(is.validTimestamp(10.4));
+    t.end();
+});
+
+tap.test('is.validTimestamp() - value is "null" - should return true', (t) => {
+    t.true(is.validTimestamp(null));
+    t.end();
+});
+
+tap.test('is.validTimestamp() - value is "undefined" - should return true', (t) => {
+    t.true(is.validTimestamp(undefined));
+    t.end();
+});
+
+tap.test('is.validTimestamp() - value is a String - should return false', (t) => {
+    t.false(is.validTimestamp('foo'));
+    t.end();
+});
+
+tap.test('is.validValue() - value is a Integer - should return true', (t) => {
+    t.true(is.validValue(10));
+    t.end();
+});
+
+tap.test('is.validValue() - value is a Double - should return false', (t) => {
+    t.false(is.validValue(10.4));
+    t.end();
+});
+
+tap.test('is.validValue() - value is "null" - should return true', (t) => {
+    t.true(is.validValue(null));
+    t.end();
+});
+
+tap.test('is.validValue() - value is "undefined" - should return true', (t) => {
+    t.true(is.validValue(undefined));
+    t.end();
+});
+
+tap.test('is.validValue() - value is a String - should return false', (t) => {
+    t.false(is.validValue('foo'));
+    t.end();
+});
+
+tap.test('is.validLabel() - value is legal - should return true', (t) => {
+    t.true(is.validLabel({ name: 'foo', value: 10 }));
+    t.end();
+});
+
+tap.test('is.validLabel() - not a label object - should return false', (t) => {
+    t.false(is.validLabel({ bar: 'a' }));
+    t.false(is.validLabel({ name: 'a', bar: 'a' }));
+    t.false(is.validLabel({ value: 'a', bar: 'a' }));
+    t.end();
+});
+
+tap.test('is.validLabel() - value of "name" property is illegal - should return false', (t) => {
+    t.false(is.validLabel({ name: '1 foo_', value: 10 }));
+    t.end();
+});
+
+tap.test('is.validLabel() - value of "value" property is illegal - should return false', (t) => {
+    t.false(is.validLabel({ name: 'foo', value: 'bar' }));
+    t.end();
+});
+
+tap.test('is.validLabels() - value is Array with legal objects - should return true', (t) => {
+    t.true(is.validLabels([{ name: 'bar', value: 5 }, { name: 'foo', value: 10 }]));
+    t.end();
+});
+
+tap.test('is.validLabels() - value is empty Array - should return true', (t) => {
+    t.true(is.validLabels([]));
+    t.end();
+});
+
+tap.test('is.validLabels() - value is not an Array - should return false', (t) => {
+    t.false(is.validLabels('foo'));
+    t.end();
+});
+
+tap.test('is.validLabels() - value is Array with some ilegal objects - should return false', (t) => {
+    t.false(is.validLabels([{ name: 'bar', value: 5 }, { name: 'xyz', value: 'a' }, { name: 'foo', value: 10 }, 'foo']));
+    t.end();
+});
--- test/metric.js
+++ test/metric.js
@@ -2,38 +2,184 @@
 
 const util = require('util');
 const tap = require('tap');
+const lolex = require('lolex');
 const Metric = require('../lib/metric');
 
 tap.test('Metric() - object type - should be Metric', (t) => {
-    const metric = new Metric();
+    const metric = new Metric({ name: 'foo', description: 'bar' });
     t.equal(Object.prototype.toString.call(metric), '[object Metric]');
     t.end();
 });
 
-tap.test('set and get value of source', (t) => {
-    const metric = new Metric({
-        name: 'valid_name',
-        description: 'Valid description',
-        timestamp: 12345,
-    });
-    metric.source = 'foo';
-    t.equal(metric.source, 'foo');
+tap.test('Metric() - No arguments - should throw', (t) => {
+    t.plan(1);
+    t.throws(() => {
+        const metric = new Metric(); // eslint-disable-line no-unused-vars
+    }, /The argument "name" must be provided/);
     t.end();
 });
 
-tap.test('optional values return null when not provided', (t) => {
-    const metric = new Metric({
-        name: 'valid_name',
-        description: 'Valid description',
-        timestamp: 12345,
-    });
-    t.equal(metric.value, null);
-    t.equal(metric.time, null);
-    t.same(metric.meta, {});
+tap.test('Metric() - "name" argument is not provided - should throw', (t) => {
+    t.plan(1);
+    t.throws(() => {
+        const metric = new Metric({ description: 'bar' }); // eslint-disable-line no-unused-vars
+    }, /The argument "name" must be provided/);
     t.end();
 });
 
-tap.test('stringifying includes all keys', (t) => {
+tap.test('Metric() - "description" argument is not provided - should throw', (t) => {
+    t.plan(1);
+    t.throws(() => {
+        const metric = new Metric({ name: 'bar' }); // eslint-disable-line no-unused-vars
+    }, /The argument "description" must be provided/);
+    t.end();
+});
+
+tap.test('Metric() - "name" argument is not valid - should throw', (t) => {
+    t.plan(1);
+    t.throws(() => {
+        const metric = new Metric({ name: '10', description: 'bar' }); // eslint-disable-line no-unused-vars
+    }, /Provided value to argument "name" is not legal/);
+    t.end();
+});
+
+tap.test('Metric() - "timestamp" argument is not valid - should throw', (t) => {
+    t.plan(1);
+    t.throws(() => {
+        const metric = new Metric({ name: 'foo', description: 'bar', timestamp: '10' }); // eslint-disable-line no-unused-vars
+    }, /Provided value to argument "timestamp" is not legal/);
+    t.end();
+});
+
+tap.test('Metric() - "description" argument is not valid - should throw', (t) => {
+    t.plan(1);
+    t.throws(() => {
+        const metric = new Metric({ name: 'foo', description: 10, }); // eslint-disable-line no-unused-vars
+    }, /Provided value to argument "description" is not legal/);
+    t.end();
+});
+
+tap.test('Metric() - "source" argument is not valid - should throw', (t) => {
+    t.plan(1);
+    t.throws(() => {
+        const metric = new Metric({ name: 'foo', description: 'bar', source: 10 }); // eslint-disable-line no-unused-vars
+    }, /Provided value to argument "source" is not legal/);
+    t.end();
+});
+
+tap.test('Metric() - "type" argument is not valid - should throw', (t) => {
+    t.plan(1);
+    t.throws(() => {
+        const metric = new Metric({ name: 'foo', description: 'bar', type: '10' }); // eslint-disable-line no-unused-vars
+    }, /Provided value to argument "type" is not legal/);
+    t.end();
+});
+
+tap.test('Metric() - "value" argument is not valid - should throw', (t) => {
+    t.plan(1);
+    t.throws(() => {
+        const metric = new Metric({ name: 'foo', description: 'bar', value: '10' }); // eslint-disable-line no-unused-vars
+    }, /Provided value to argument "value" is not legal/);
+    t.end();
+});
+
+tap.test('Metric() - only required arguments is set - should set defaults on optional arguments', (t) => {
+    const clock = lolex.install();
+    clock.tick(1547507561724);
+
+    const metric = new Metric({
+        name: 'valid_name',
+        description: 'Valid description',
+    });
+
+    t.equal(metric.timestamp, (1547507561724 / 1000));
+    t.equal(metric.type, 0);
+    t.same(metric.labels, []);
+    t.equal(metric.source, null);
+    t.equal(metric.meta, null);
+
+    clock.uninstall();
+
+    t.end();
+});
+
+tap.test('Metric() - all arguments is set - should set on respectative property', (t) => {
+    const metric = new Metric({
+        description: 'Valid description',
+        timestamp: 1547507561724,
+        source: 'valid_source',
+        labels: [{ name: 'foo', value: 5 }],
+        value: 10,
+        meta: 'valid_meta',
+        type: 2,
+        name: 'valid_name',
+    });
+
+    t.equal(metric.description, 'Valid description');
+    t.equal(metric.timestamp, 1547507561724);
+    t.equal(metric.source, 'valid_source');
+    t.same(metric.labels, [{ name: 'foo', value: 5 }]);
+    t.equal(metric.value, 10);
+    t.equal(metric.type, 2);
+    t.equal(metric.meta, 'valid_meta');
+    t.equal(metric.name, 'valid_name');
+
+    t.end();
+});
+
+tap.test('Metric.toJSON() - only required arguments is set - should contain all properties with defaults', (t) => {
+    const clock = lolex.install();
+    clock.tick(1547507561724);
+
+    const metric = new Metric({
+        name: 'valid_name',
+        description: 'Valid description',
+    });
+
+    const serialized = metric.toJSON();
+
+    t.equal(serialized.description, 'Valid description');
+    t.equal(serialized.timestamp, (1547507561724 / 1000));
+    //    t.equal(serialized.source, null);
+    t.same(serialized.labels, []);
+    t.equal(serialized.value, null);
+    t.equal(serialized.type, 0);
+    t.equal(serialized.meta, null);
+    t.equal(metric.name, 'valid_name');
+
+    clock.uninstall();
+
+    t.end();
+});
+
+tap.test('Metric.toJSON() - all arguments is set - should contain all properties with set values', (t) => {
+    const metric = new Metric({
+        description: 'Valid description',
+        timestamp: 1547507561724,
+        source: 'valid_source',
+        labels: [{ name: 'foo', value: 5 }],
+        value: 10,
+        meta: 'valid_meta',
+        type: 2,
+        name: 'valid_name',
+    });
+
+    const serialized = metric.toJSON();
+
+    t.equal(serialized.description, 'Valid description');
+    t.equal(serialized.timestamp, 1547507561724);
+    //    t.equal(serialized.source, 'valid_source');
+    t.same(serialized.labels, [{ name: 'foo', value: 5 }]);
+    t.equal(serialized.value, 10);
+    t.equal(serialized.type, 2);
+    t.equal(serialized.meta, 'valid_meta');
+    t.equal(serialized.name, 'valid_name');
+
+    t.end();
+});
+
+
+tap.test('Metric.toPrimitive() - stringify - should includes all keys', (t) => {
     const metric = new Metric({
         name: 'valid_name',
         description: 'Valid description',
@@ -41,82 +187,12 @@ tap.test('stringifying includes all keys', (t) => {
     });
     t.equal(
         `${metric}`,
-        'Metric {"name":"valid_name","description":"Valid description","timestamp":12345,"type":0,"value":null,"time":null,"meta":{}}',
+        'Metric {"name":"valid_name","description":"Valid description","timestamp":12345,"type":0,"value":null,"labels":[],"meta":null}',
     );
     t.end();
 });
 
-tap.test('omitting required keys results in undefined instead of "null"', (t) => {
-    const metric = new Metric({
-        name: 'valid_name',
-    });
-    t.equal(metric.name, 'valid_name');
-    t.equal(metric.description, undefined);
-    t.equal(metric.timestamp, undefined);
-    t.equal(metric.type, 0);
-    t.equal(metric.time, null);
-    t.equal(metric.value, null);
-    t.same(metric.meta, {});
-    t.end();
-});
-
-tap.test('the number 0 is treated as a number and does not yeld "null"', (t) => {
-    const metric = new Metric({
-        name: 'valid_name',
-        value: 0,
-        type: 0,
-        timestamp: 0,
-    });
-    t.equal(metric.timestamp, 0);
-    t.equal(metric.type, 0);
-    t.equal(metric.value, 0);
-    t.end();
-});
-
-tap.test('empty string value is treated as "null"', (t) => {
-    const metric = new Metric({
-        name: 'valid_name',
-        value: '',
-    });
-    t.equal(metric.value, null);
-    t.end();
-});
-
-tap.test(
-    'empty string value, consist of multiple whitespaces, is treated as "null"',
-    (t) => {
-        const metric = new Metric({
-            name: 'valid_name',
-            value: '   ',
-        });
-        t.equal(metric.value, null);
-        t.end();
-    },
-);
-
-tap.test('util.inspect includes all keys', (t) => {
-    const metric = new Metric({
-        name: 'valid_name',
-        description: 'Valid description',
-        timestamp: 12345,
-        time: 12345,
-        value: 123,
-        meta: { key: 'value' },
-    });
-
-    const result = `Metric { name: 'valid_name',
-  description: 'Valid description',
-  timestamp: 12345,
-  type: 0,
-  value: 123,
-  time: 12345,
-  meta: { key: 'value' } }`;
-
-    t.equal(util.inspect(metric), result);
-    t.end();
-});
-
-tap.test('toStringTag includes Metric in name', (t) => {
+tap.test('Metric.toPrimitive() - stringify - should include "Metric" in name', (t) => {
     const metric = new Metric({
         name: 'valid_name',
         description: 'Valid description',
@@ -126,17 +202,9 @@ tap.test('toStringTag includes Metric in name', (t) => {
     t.end();
 });
 
-tap.test('toString outputs a full stringified object', (t) => {
-    const metric = new Metric({
-        name: 'valid_name',
-        description: 'Valid description',
-        timestamp: 12345,
-    });
-    t.equal(metric.toString(), '[object Metric]');
-    t.end();
-});
+tap.test('Metric.toPrimitive() - defaults - should throw', (t) => {
+    t.plan(1);
 
-tap.test('parseInt on metric instance outputs null', (t) => {
     const metric = new Metric({
         name: 'valid_name',
         description: 'Valid description',
@@ -145,6 +213,73 @@ tap.test('parseInt on metric instance outputs null', (t) => {
 
     t.throws(() => {
         const foo = metric + 1; // eslint-disable-line no-unused-vars
-    }, new Error('Invalid usage. Metric class instance cannot be treated as type "default"'));
+    }, /Invalid usage. Metric class instance cannot be treated as type "default"/);
+    t.end();
+});
+
+tap.test('Metric() - util.inspect - should includes all keys', (t) => {
+    const metric = new Metric({
+        name: 'valid_name',
+        description: 'Valid description',
+        timestamp: 12345,
+        value: 123,
+        meta: { key: 'value' },
+    });
+
+    const result = `Metric { name: 'valid_name',
+  description: 'Valid description',
+  timestamp: 12345,
+  type: 0,
+  value: 123,
+  labels: [],
+  meta: { key: 'value' } }`;
+
+    t.equal(util.inspect(metric), result);
+    t.end();
+});
+
+tap.test('Metric() - 0 values - should treat the number 0 as a number and not yeld "null"', (t) => {
+    const metric = new Metric({
+        name: 'valid_name',
+        description: 'Valid description',
+        value: 0,
+        type: 0,
+    });
+    t.equal(metric.type, 0);
+    t.equal(metric.value, 0);
+    t.end();
+});
+
+tap.test('Metric() - empty string - should be treated as "null"', (t) => {
+    const metric = new Metric({
+        name: 'valid_name',
+        description: 'Valid description',
+        source: '',
+    });
+    t.equal(metric.source, null);
+    t.end();
+});
+
+tap.test(
+    'Metric() - empty string, consist of multiple whitespaces - should be treated as "null"',
+    (t) => {
+        const metric = new Metric({
+            name: 'valid_name',
+            description: 'Valid description',
+            source: '     ',
+        });
+        t.equal(metric.source, null);
+        t.end();
+    },
+);
+
+tap.test('Metric.source - set value - should set the value', (t) => {
+    const metric = new Metric({
+        name: 'valid_name',
+        description: 'Valid description',
+        timestamp: 12345,
+    });
+    metric.source = 'foo';
+    t.equal(metric.source, 'foo');
     t.end();
 });
